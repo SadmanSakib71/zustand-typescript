67a3b6e0dcab855c5067312fa75dd5c533cb05ed
Sadman Sakib <EMAIL_LINKED_TO_SadmanSakib71>
2026-01-28 16:53:48 +0600

    zustand-simple-use

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { create } from "zustand";
+import { Button } from "./components/ui/button";
+
+const useStore = create<{
+  count: number;
+  increment: () => void;
+  decrement: () => void;
+}>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state?.count + 1 })),
+  decrement: () => set((state) => ({ count: state?.count - 1 })),
+}));
+
 function App() {
+  const store = useStore();
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button onClick={store.increment}>+</Button>
+      <Count />
+      <Button onClick={store.decrement}>-</Button>
     </>
   );
+}
+
+function Count() {
+  const store = useStore();
+  return <>{store.count}</>;
 }
 
 export default App;
