95c575bcd36d3aa8ef0a4102a4d8a63d18c4f6e0
Sadman Sakib <EMAIL_LINKED_TO_SadmanSakib71>
2026-01-30 17:19:27 +0600

    parsist data

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,20 @@
 import type { store } from "@/types/store";
 import { create } from "zustand";
-import { devtools, subscribeWithSelector } from "zustand/middleware";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { createCartSlice } from "./cart-slice";
 import { createUserSlice } from "./user-slice";
 
 export const useStore = create<store>()(
   devtools(
-    subscribeWithSelector(
-      immer((...a) => ({
-        ...createUserSlice(...a),
-        ...createCartSlice(...a),
-      })),
+    persist(
+      subscribeWithSelector(
+        immer((...a) => ({
+          ...createUserSlice(...a),
+          ...createCartSlice(...a),
+        })),
+      ),
+      { name: "local-storage" },
     ),
   ),
 );
